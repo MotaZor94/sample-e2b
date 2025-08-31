693f3426fa5f5acb59a06114ba56cf35d4bd4058
MotaZor94 <45624002+MotaZor94@users.noreply.github.com>
2025-08-31 09:58:24 +0200

    Increase desired, max, and min size for server nodes

--- infra-iac/terraform/main.tf
+++ infra-iac/terraform/main.tf
@@ -53,9 +53,9 @@ locals {
     server = {
       instance_type_x86    = "t3.medium"
       instance_type_arm    = "t4g.medium"
-      desired_capacity = 1
-      max_size         = 1
-      min_size         = 1
+      desired_capacity = 2
+      max_size         = 2
+      min_size         = 2
     }
     # Client nodes run workloads and containers
     client = {
